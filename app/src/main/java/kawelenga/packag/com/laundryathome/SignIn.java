99316519c430726666e46e2ec68959fe74ce9b04
vickaw <kawelengav@gmail.com>
2019-10-08 21:29:42 +0200

    Added Navagition

--- app/src/main/java/kawelenga/packag/com/laundryathome/SignIn.java
+++ app/src/main/java/kawelenga/packag/com/laundryathome/SignIn.java
@@ -2,9 +2,14 @@ package kawelenga.packag.com.laundryathome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+
+    private Button loginIntoApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,19 @@ public class SignIn extends AppCompatActivity {
 
         getSupportActionBar().setTitle(" ");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        loginIntoApp= findViewById(R.id.btnLogin);
+
+        loginIntoApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent loginin = new Intent(SignIn.this, ScheduleWash.class);
+                startActivity(loginin);
+
+
+            }
+        });
 
     }
 }
