46d603f1a080c92448b109f055c50b238f49619c
vickaw <kawelengav@gmail.com>
2019-10-10 17:46:46 +0200

    G+ an Facebook correct

---
+++ app/src/main/java/kawelenga/packag/com/laundryathome/PickupAddress.java
@@ -0,0 +1,14 @@
+package kawelenga.packag.com.laundryathome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickupAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pickup_address);
+    }
+}
